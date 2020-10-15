3153e45fc57fc8664d6a22deb4b30deb84654715
YannickMontes <montes.yannick@gmail.com>
2020-10-15 12:35:25 +0200

    Initial commit

---
+++ Assets/Scripts/LocalInputManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalInputManager : Yube.InputManager<LocalInputManager.EAxis, LocalInputManager.EKey>
+{
+	public enum EAxis
+	{
+		HORIZONTAL,
+		VERTICAL,
+		FIRE1
+	}
+
+	public enum EKey
+	{
+	}
+}
