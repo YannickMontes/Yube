21fa6e2f7386d3bcf18a0352821646952fc36105
YannickMontes <montes.yannick@gmail.com>
2022-10-18 15:02:42 +0200

    Enum array implementation

---
+++ Editor/System/EnumArrayPropertyDrawer.cs
@@ -0,0 +1,47 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(EnumArray<,>), true)]
+public class EnumArrayPropertyDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        float baseHeight = base.GetPropertyHeight(property, label);
+        if(foldoutOpen)
+        {
+            SerializedProperty namesProp = property.FindPropertyRelative(namesPropName);
+            return baseHeight + (namesProp.arraySize) * EditorGUIUtility.singleLineHeight + (space * namesProp.arraySize - 1);
+        }
+        else
+        {
+            return baseHeight;
+        }
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        Rect newPosition = position;
+        newPosition.height = EditorGUIUtility.singleLineHeight;
+        foldoutOpen = EditorGUI.Foldout(newPosition, foldoutOpen, new GUIContent(property.displayName), true);
+        if(foldoutOpen)
+        {
+            EditorGUI.indentLevel++;
+            newPosition.y += EditorGUIUtility.singleLineHeight;
+
+            SerializedProperty namesProp = property.FindPropertyRelative(namesPropName);
+            SerializedProperty valuesProp = property.FindPropertyRelative(valuesPropName);
+
+            for (int i = 0; i < namesProp.arraySize; i++)
+            {
+                EditorGUI.PropertyField(newPosition, valuesProp.GetArrayElementAtIndex(i), new GUIContent(namesProp.GetArrayElementAtIndex(i).stringValue));
+                newPosition.y += EditorGUIUtility.singleLineHeight + (i + 1 < namesProp.arraySize ? space : 0);
+            }
+            EditorGUI.indentLevel--; 
+        }
+    }
+
+    private bool foldoutOpen = false;
+    private float space = 3;
+    private string namesPropName = "names";
+    private string valuesPropName = "values";
+}
---
+++ Runtime/System/EnumArray.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Collections.LowLevel.Unsafe;
+
+public abstract class EnumArray<ENUM, CLASS> : ISerializationCallbackReceiver where ENUM : struct, IConvertible where CLASS : class 
+{
+    public CLASS this[ENUM enumValue] { get => values[UnsafeUtility.EnumToInt(enumValue)]; }
+    public CLASS this[int index] { get => values[index]; }
+    public CLASS this[uint index] { get => values[(int)index]; }
+
+    public EnumArray()
+    {
+        string[] enumNames = Enum.GetNames(typeof(ENUM));
+        if(enumNames.Length == 0)
+        {
+            Debug.LogError($"ERROR: Can't make enum array of enum {typeof(ENUM)} if this enum has nos values !");
+            return;
+        }
+        foreach(string name in enumNames)
+        {
+            names.Add(name);
+            values.Add(null);
+        }
+    }
+
+    #region Private
+
+    [SerializeField]
+    private List<string> names = new List<string>();
+    [SerializeField]
+    private List<CLASS> values = new List<CLASS>();
+
+    public void OnBeforeSerialize()
+    {
+    }
+
+    public void OnAfterDeserialize()
+    {
+#if UNITY_EDITOR
+        string[] enumNames = Enum.GetNames(typeof(ENUM));
+        if(names.Count != enumNames.Length)
+        {
+            for (int i = 0; i < enumNames.Length; i++)
+            {
+                if(i >= names.Count)
+                {
+                    names.Add(enumNames[i]);
+                    values.Add(null);
+                }
+                else if(names[i] != enumNames[i])
+                {
+                    names.Insert(i, enumNames[i]);
+                    values.Insert(i, null);
+                }
+            }
+        }
+#endif
+    }
+
+    #endregion
+}
