3e1b974501eddf35409364ac5ae98cd12b107da6
YannickMontes <montes.yannick@gmail.com>
2022-10-18 15:06:42 +0200

    Forgot namespaces

--- Editor/System/EnumArrayPropertyDrawer.cs
+++ Editor/System/EnumArrayPropertyDrawer.cs
@@ -1,47 +1,50 @@
 using UnityEditor;
 using UnityEngine;
 
-[CustomPropertyDrawer(typeof(EnumArray<,>), true)]
-public class EnumArrayPropertyDrawer : PropertyDrawer
+namespace Yube
 {
-    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    [CustomPropertyDrawer(typeof(EnumArray<,>), true)]
+    public class EnumArrayPropertyDrawer : PropertyDrawer
     {
-        float baseHeight = base.GetPropertyHeight(property, label);
-        if(foldoutOpen)
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            SerializedProperty namesProp = property.FindPropertyRelative(namesPropName);
-            return baseHeight + (namesProp.arraySize) * EditorGUIUtility.singleLineHeight + (space * namesProp.arraySize - 1);
-        }
-        else
-        {
-            return baseHeight;
-        }
-    }
-
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-    {
-        Rect newPosition = position;
-        newPosition.height = EditorGUIUtility.singleLineHeight;
-        foldoutOpen = EditorGUI.Foldout(newPosition, foldoutOpen, new GUIContent(property.displayName), true);
-        if(foldoutOpen)
-        {
-            EditorGUI.indentLevel++;
-            newPosition.y += EditorGUIUtility.singleLineHeight;
-
-            SerializedProperty namesProp = property.FindPropertyRelative(namesPropName);
-            SerializedProperty valuesProp = property.FindPropertyRelative(valuesPropName);
-
-            for (int i = 0; i < namesProp.arraySize; i++)
+            float baseHeight = base.GetPropertyHeight(property, label);
+            if (foldoutOpen)
             {
-                EditorGUI.PropertyField(newPosition, valuesProp.GetArrayElementAtIndex(i), new GUIContent(namesProp.GetArrayElementAtIndex(i).stringValue));
-                newPosition.y += EditorGUIUtility.singleLineHeight + (i + 1 < namesProp.arraySize ? space : 0);
+                SerializedProperty namesProp = property.FindPropertyRelative(namesPropName);
+                return baseHeight + (namesProp.arraySize) * EditorGUIUtility.singleLineHeight + (space * namesProp.arraySize - 1);
             }
-            EditorGUI.indentLevel--; 
+            else
+            {
+                return baseHeight;
+            }
         }
-    }
 
-    private bool foldoutOpen = false;
-    private float space = 3;
-    private string namesPropName = "names";
-    private string valuesPropName = "values";
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            Rect newPosition = position;
+            newPosition.height = EditorGUIUtility.singleLineHeight;
+            foldoutOpen = EditorGUI.Foldout(newPosition, foldoutOpen, new GUIContent(property.displayName), true);
+            if (foldoutOpen)
+            {
+                EditorGUI.indentLevel++;
+                newPosition.y += EditorGUIUtility.singleLineHeight;
+
+                SerializedProperty namesProp = property.FindPropertyRelative(namesPropName);
+                SerializedProperty valuesProp = property.FindPropertyRelative(valuesPropName);
+
+                for (int i = 0; i < namesProp.arraySize; i++)
+                {
+                    EditorGUI.PropertyField(newPosition, valuesProp.GetArrayElementAtIndex(i), new GUIContent(namesProp.GetArrayElementAtIndex(i).stringValue));
+                    newPosition.y += EditorGUIUtility.singleLineHeight + (i + 1 < namesProp.arraySize ? space : 0);
+                }
+                EditorGUI.indentLevel--;
+            }
+        }
+
+        private bool foldoutOpen = false;
+        private float space = 3;
+        private string namesPropName = "names";
+        private string valuesPropName = "values";
+    }
 }
--- Runtime/System/EnumArray.cs
+++ Runtime/System/EnumArray.cs
@@ -3,60 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Collections.LowLevel.Unsafe;
 
-public abstract class EnumArray<ENUM, CLASS> : ISerializationCallbackReceiver where ENUM : struct, IConvertible where CLASS : class 
+namespace Yube
 {
-    public CLASS this[ENUM enumValue] { get => values[UnsafeUtility.EnumToInt(enumValue)]; }
-    public CLASS this[int index] { get => values[index]; }
-    public CLASS this[uint index] { get => values[(int)index]; }
-
-    public EnumArray()
+    public abstract class EnumArray<ENUM, CLASS> : ISerializationCallbackReceiver where ENUM : struct, IConvertible where CLASS : class
     {
-        string[] enumNames = Enum.GetNames(typeof(ENUM));
-        if(enumNames.Length == 0)
+        public CLASS this[ENUM enumValue] { get => values[UnsafeUtility.EnumToInt(enumValue)]; }
+        public CLASS this[int index] { get => values[index]; }
+        public CLASS this[uint index] { get => values[(int)index]; }
+
+        public EnumArray()
         {
-            Debug.LogError($"ERROR: Can't make enum array of enum {typeof(ENUM)} if this enum has nos values !");
-            return;
-        }
-        foreach(string name in enumNames)
-        {
-            names.Add(name);
-            values.Add(null);
-        }
-    }
-
-    #region Private
-
-    [SerializeField]
-    private List<string> names = new List<string>();
-    [SerializeField]
-    private List<CLASS> values = new List<CLASS>();
-
-    public void OnBeforeSerialize()
-    {
-    }
-
-    public void OnAfterDeserialize()
-    {
-#if UNITY_EDITOR
-        string[] enumNames = Enum.GetNames(typeof(ENUM));
-        if(names.Count != enumNames.Length)
-        {
-            for (int i = 0; i < enumNames.Length; i++)
+            string[] enumNames = Enum.GetNames(typeof(ENUM));
+            if (enumNames.Length == 0)
             {
-                if(i >= names.Count)
-                {
-                    names.Add(enumNames[i]);
-                    values.Add(null);
-                }
-                else if(names[i] != enumNames[i])
-                {
-                    names.Insert(i, enumNames[i]);
-                    values.Insert(i, null);
-                }
+                Debug.LogError($"ERROR: Can't make enum array of enum {typeof(ENUM)} if this enum has nos values !");
+                return;
+            }
+            foreach (string name in enumNames)
+            {
+                names.Add(name);
+                values.Add(null);
             }
         }
-#endif
-    }
 
-    #endregion
+        #region Private
+
+        [SerializeField]
+        private List<string> names = new List<string>();
+        [SerializeField]
+        private List<CLASS> values = new List<CLASS>();
+
+        public void OnBeforeSerialize()
+        {
+        }
+
+        public void OnAfterDeserialize()
+        {
+#if UNITY_EDITOR
+            string[] enumNames = Enum.GetNames(typeof(ENUM));
+            if (names.Count != enumNames.Length)
+            {
+                for (int i = 0; i < enumNames.Length; i++)
+                {
+                    if (i >= names.Count)
+                    {
+                        names.Add(enumNames[i]);
+                        values.Add(null);
+                    }
+                    else if (names[i] != enumNames[i])
+                    {
+                        names.Insert(i, enumNames[i]);
+                        values.Insert(i, null);
+                    }
+                }
+            }
+#endif
+        }
+
+        #endregion
+    }
 }
